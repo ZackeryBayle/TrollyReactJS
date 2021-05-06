70c69e44bfa9e69cae5b1d089fa086c0cd383309
Zackery Bayle <Zackery.bayle@gmail.com>
2021-05-06 08:59:17 -0700

    init commit for staging-issues

--- src/helper/fetch.js
+++ src/helper/fetch.js
@@ -1,26 +1,82 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Fetching(props) {
-    // const [userData, setUserData] = useState({});
-    // const [fetchError, setFetchError] = useState();
 
-    const search = (props) => {
+    const [userData, setUserData] = useState({
+        firsName:"",
+        lastName:"",
+        email:"",
+        serial:null
+    });
+    
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [serial, setSerial] = useState(null);
 
-        fetch(`http://localhost:8080/api/Users/serial/${props.target.value}`)
-            .then(res => res.json())
-            .then(
-            (result) => {
-                console.log(result);
-            },
-            (error) => {
-                console.log(error)
-            }
-            )
+    const updateSearch = (e) => {
+        e.preventDefault();
+        if (e.target.id === "firstName"){
+            setFirstName(e.target.value);
+        }
+        if (e.target.id === "lastName"){
+            setLastName(e.target.value);
+        }
+        if (e.target.id === "email"){
+            setEmail(e.target.value);
+        }
+        if (e.target.id === "serial"){
+            setSerial(e.target.value);
+        }
+
+        setUserData({
+            "firstName":firstName,
+            "lastName":lastName,
+            "email":email,
+            "serial":serial
+        })
     }
-    return (
+    
 
-        <div onLoad={search}>
 
-        </div>
-    );
+    const searchFormSubmit = (e) => {
+        console.log("Submit Search Form...")
+
+        const nameSearch = (e) => {
+            axios.get(`http://localhost:8080/api/Users/name/${userData.firstName} ${userData.lastName}`)
+                .then(response => {
+                    console.log(response)
+                    setUserData(response.data)
+                })
+                .catch( error => {
+                    console.log(error)
+                })
+        }
+
+
+
+        const emailSearch =(e) => {
+            axios.get(`http://localhost:8080/api/Users/email/${userData.email}`)
+                .then( response => {
+                // console.log(response.data);
+                setUserData(response.data)
+                })
+                .catch( error => {
+                    console.log(error)
+                })
+        }
+        
+
+        if(!userData.firstName){
+            nameSearch(e);
+        }
+
+        if(userData.email){
+            emailSearch(e);
+        }
+ 
+ 
+    }
+
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,182 +1,125 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Accordion, Button, Card, Col, Form, FormControl, InputGroup } from "react-bootstrap";
 
 import Notes from '../components/note-accordion';
 import Fetching from '../helper/fetch';
+import UpdateHook from '../helper/update_hook';
+import FormComponent from '../components/form-component';
 
 import axios from 'axios'
 
 export default function Home(){
 
-    const [userData, setUserData] = useState({
-        firsName:"",
-        lastName:"",
-        email:"",
-        serial:null
-    });
+    // const [userData, setUserData] = useState({
+    //     firsName:"",
+    //     lastName:"",
+    //     email:"",
+    //     serial:null
+    // });
     
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [serial, setSerial] = useState(null);
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [serial, setSerial] = useState(null);
 
 
 
 
-    const [formGridCrossCheck, setformGridCrossCheck] = useState(false);
-    const [validated, setValidated] = useState(false);
-    const [currentValue, setCurrentValue] = useState([]);
-    const [fetchError, setFetchError] = useState();
-    const [ownerNotes, setOwnerNotes] = useState([])
-
-
-
-
-
-    // Check for Cross Check - use more then one field to search for user
-
-    const crossCheckSub = (e) =>{
-        if (formGridCrossCheck === true){
-            setformGridCrossCheck(false);
-        }else setformGridCrossCheck(true)
-        console.log({formGridCrossCheck});
-    }
-
-    const updateSearch = (e) => {
-        e.preventDefault();
-        if (e.target.id === "firstName"){
-            setFirstName(e.target.value);
-        }
-        if (e.target.id === "lastName"){
-            setLastName(e.target.value);
-        }
-       /* if (e.target.id === "email"){
-            setEmail(e.target.value);
-        }*/
-        if (e.target.id === "serial"){
-            setSerial(e.target.value);
-        }
-
-        setUserData({
-            "firstName":firstName,
-            "lastName":lastName,
-            "email":email,
-            "serial":serial
-        })
-    }
-
-    const updateSearchEmail = (e) => {
-        if (e.target.id === "email"){
-            setEmail(e.target.value);
-        }
-
-    }
+    // const [formGridCrossCheck, setformGridCrossCheck] = useState(false);
     
-
-
-    const searchFormSubmit = (e) => {
-        console.log("Submit Search Form...")
-
-        const nameSearch = (e) => {
-            axios.get(`http://localhost:8080/api/Users/name/${userData.firstName} ${userData.lastName}`)
-                .then(response => {
-                    console.log(response)
-                    setUserData(response.data)
-                })
-                .catch( error => {
-                    console.log(error)
-                })
-        }
+    // const [currentValue, setCurrentValue] = useState
+    // ({
+    //     email:String,
+    //     firstName:String,
+    //     lastName:String,
+    //     serial:Number
+    // });
+    // const [fetchError, setFetchError] = useState();
+    // const [ownerNotes, setOwnerNotes] = useState([])
 
 
 
-        const emailSearch =(e) => {
-            axios.get(`http://localhost:8080/api/Users/email/${userData.email}`)
-                .then( response => {
-                // console.log(response.data);
-                setUserData(response.data)
-                })
-                .catch( error => {
-                    console.log(error)
-                })
-        }
-        
 
-        if(!userData.firstName){
-            nameSearch(e);
-        }
 
-        if(userData.email){
-            emailSearch(e);
-        }
+    // // Check for Cross Check - use more then one field to search for user
+
+    // const crossCheckSub = (e) =>{
+    //     const check = e.target.value;
+
+    //     // crossCheckSearch()
+
+    // }
+
+    // const searchFormSubmit = (props) => {
+    //     console.log("Submit Search Form...")
+    //     setCurrentValue(props,
+    //         {
+    //             "email": currentValue.email
+    //         }
+    //     );
+    //     console.log(currentValue + "CurrentValue");
+
+    //     const nameSearch = (e) => {
+    //         axios.get(`http://localhost:8080/api/Users/name/${currentValue.firstName} ${currentValue.lastName}`)
+    //             .then(response => {
+    //                 console.log(response)
+    //                 // setUserData(response.data)
+    //             })
+    //             .catch( error => {
+    //                 console.log(error)
+    //             })
+    //     }
+
+
+
+    //     const emailSearch =(e) => {
+    //         axios.get(`http://localhost:8080/api/Users/email/${currentValue.email}`)
+    //             .then( response => {
+    //             // console.log(response.data);
+    //             // setUserData(response.data)
+    //             })
+    //             .catch( error => {
+    //                 console.log(error)
+    //             })
+    //     }
+    // }
+
+    // const handleChange = (e) => {
+    //     setCurrentValue({
+    //         "email": "test"
+    //     })
+    // }
  
- 
-    }
+
 
 
     return(
 
         <div className="home_container">
             <div className="home_contact_container">
-                <div className="home_search_container">
-                    <Form className="Owner_serch" noValidate validated={validated} >
-                        <Form.Row>
-                            <Form.Group as={Col} >
-                                <Form.Label></Form.Label>
-                                <Form.Control id="email" type="email" placeholder="Enter Email" onChange={updateSearchEmail} />
-                            </Form.Group>
-
-                            <Form.Group as={Col} >
-                                <Form.Label></Form.Label>
-                                <Form.Control id="firstName" type="name" placeholder="Enter First Name" onChange={updateSearch} />
-                                <Form.Label></Form.Label>
-                                <Form.Control id="lastName" type="name" placeholder="Enter Last Name" onChange={updateSearch} />
-                            </Form.Group>
-
-                            <Form.Group as={Col} >
-                                <Form.Label></Form.Label>
-                                <Form.Control id="serial" type="text" placeholder="Enter Serial" onChange={updateSearch} />
-                            </Form.Group>
-                        </Form.Row>
-
-                        <Form.Group id="formGridCrossCheck">
-                            <Form.Check type="switch" label="Cross Check" id="crossCheck" onChange={crossCheckSub} />
-                        </Form.Group>
-
-                        <Button onClick={searchFormSubmit}>Search</Button>
-
-                    </Form>
-                    
-
-                    <div className="home_important_snips">
-                        <InputGroup>
-                            <FormControl as="textarea" aria-label="" />
-                        </InputGroup>
-                    </div>
-                </div>
-
-
+                    {/* <FormComponent /> */}
                 <div className="home_contact_log">
                     <div className="home_logs">
                         <ul>
                             <li>
-                                {userData ?
+                                {/* {this.props.userData ?
                                     (
-                                        <Notes user={userData} />
+                                        <Notes user={this.props.userData} />
                                     ): null
-                                }
+                                } */}
+                                {/* <Notes user={""}/> */}
                             </li>
                         </ul>
 
 
                     </div>
                     <div className="Hone_notes">
-                    <InputGroup>
+                    {/* <InputGroup>
                         <InputGroup.Prepend>
                         <InputGroup.Text>Notes</InputGroup.Text>
                         </InputGroup.Prepend>
                         <FormControl as="textarea" aria-label="With textarea" />
-                    </InputGroup>
+                    </InputGroup> */}
 
                     </div>
                 </div>
